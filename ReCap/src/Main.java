ae9283c1f942e4d1e6295084a8fce09456e232fe
Ahmet Can <113761538+Ahmetcanaydg@users.noreply.github.com>
2022-09-18 17:38:08 +0300

    Add files via upload

---
+++ ReCap/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi1 = 19; 
+		int sayi2 = 17 ;
+		int sayi3 = 1 ;
+		int enBuyuk = sayi1 ;
+		
+		if (enBuyuk<sayi2) {
+			
+			enBuyuk=sayi2 ;
+		}
+		
+		if (enBuyuk<sayi3) {
+			
+			enBuyuk =sayi3 ;
+		}
+		
+				System.out.println("En Büyük : "+enBuyuk);
+
+	}
+
+}
