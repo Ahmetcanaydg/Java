0c2197172aca15569ba7526396ef195b8374cdc8
Ahmet Can <113761538+Ahmetcanaydg@users.noreply.github.com>
2022-09-18 17:00:40 +0300

    Add files via upload

---
+++ Conditionals/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 19; 
+		
+		if (sayi<20) {
+			
+			System.out.println("Sayı 20'den Küçüktür.");
+		
+		} else if (sayi==20) {
+			
+			System.out.println("Sayı 20'ye Eşittir.");
+			
+		} else {
+			
+			System.out.println("SAyı 20'den Büyüktür.");
+		}
+
+				
+		
+		
+
+ }
+}
