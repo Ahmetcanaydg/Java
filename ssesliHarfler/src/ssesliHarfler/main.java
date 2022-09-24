2fb6c57bc41239525800c40680d03157b2729b6e
Ahmet Can <113761538+Ahmetcanaydg@users.noreply.github.com>
2022-09-24 16:38:07 +0300

    Add files via upload

---
+++ ssesliHarfler/src/ssesliHarfler/main.java
@@ -0,0 +1,31 @@
+package ssesliHarfler;
+
+public class main {
+
+	public static void main(String[] args) {
+		char harf = 'A';
+		
+				switch (harf) {
+				case  'A':
+				case  'I':
+				case  'U':
+				case  'O':
+				System.out.println("Kalın sesli harf") ;
+				return ;
+				}
+		
+		switch (harf) {
+		case  'E':
+		case  'İ':
+		case  'Ü':
+		case  'Ö':
+		System.out.println("İnce sesli harf") ;
+		return ;
+				
+
+	}
+
+		System.out.println ("Girdiğiniz Harf Sesli değil");
+		
+	}		
+}
