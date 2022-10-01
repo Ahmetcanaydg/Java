4f97f2abf12c21a3e9306c2542d789462c9fa429
Ahmet Can <113761538+Ahmetcanaydg@users.noreply.github.com>
2022-10-02 00:22:04 +0300

    Add files via upload

--- methods2/src/methods2/Main.java
+++ methods2/src/methods2/Main.java
@@ -6,9 +6,10 @@ public class Main {
 		String mesaj ="Bugün hava çok güzel ";
 		String yeniMesaj = sehirVer();
 		System.out.println(yeniMesaj);
-		int sayı = topla (10,7);
+		int sayı = topla (15,7);
 		System.out.println(sayı);
-		
+		int toplam = topla2(2,3,4,5,6,10);		
+		System.out.println (toplam);
 
 	}
 	public static void ekle () {
@@ -22,10 +23,25 @@ public class Main {
 		
 		System.out.println("güncellendi");
 	}public static int topla (int sayı1,int sayı2) {
-		 
+		
 		return sayı1+sayı2 ;
 		
-	}public static String sehirVer () {
+	}public static int topla2 (int...sayılar) {
+		int toplam = 0;
+	for (int sayı:sayılar ) {
+		
+		toplam+=sayı ;
+		
+	}return toplam;
+		
+		
+	}
+		 
+	
+	
+	
+	
+	public static String sehirVer () {
 		return "Ankara" ;
 	}
 }
